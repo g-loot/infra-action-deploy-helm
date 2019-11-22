ae5018b1d1a30553a2e07fd7fb340dd466e5fa59
Simon Schneider <simon.schneider@gloot.com>
2019-11-22 14:30:09 +0100

    fix: protect against env variable bash magic by adding quotes

--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 set -e
-export GCP_KEY=$INPUT_GCP_KEY
-export ZONE=$INPUT_ZONE
-export PROJECT=$INPUT_PROJECT
-export CLUSTER=$INPUT_CLUSTER
-export GCS_HELM_REPO=$INPUT_GCS_HELM_REPO
-export HELM_VERSION=$INPUT_HELM_VERSION
-export HELM_ARGS=$INPUT_HELM_ARGS
-export HELM_COMMAND=$INPUT_HELM_COMMAND
+export GCP_KEY="$INPUT_GCP_KEY"
+export ZONE="$INPUT_ZONE"
+export PROJECT="$INPUT_PROJECT"
+export CLUSTER="$INPUT_CLUSTER"
+export GCS_HELM_REPO="$INPUT_GCS_HELM_REPO"
+export HELM_VERSION="$INPUT_HELM_VERSION"
+export HELM_ARGS="$INPUT_HELM_ARGS"
+export HELM_COMMAND="$INPUT_HELM_COMMAND"
 
 echo -----
-echo $ZONE
-echo $PROJECT
-echo $CLUSTER
-echo $GCS_HELM_REPO
-echo $HELM_VERSION
-echo $HELM_ARGS
-echo $HELM_COMMAND
+echo "$ZONE"
+echo "$PROJECT"
+echo "$CLUSTER"
+echo "$GCS_HELM_REPO"
+echo "$HELM_VERSION"
+echo "$HELM_ARGS"
+echo "$HELM_COMMAND"
 echo -----
 
-echo $GCP_KEY | base64 -d > /tmp/google_credentials.json
+echo "$GCP_KEY" | base64 -d > /tmp/google_credentials.json
 gcloud auth activate-service-account --key-file /tmp/google_credentials.json
 
 export HELM_HOME=/helm2home
-helm2 repo add gcs-repo $GCS_HELM_REPO
+helm2 repo add gcs-repo "$GCS_HELM_REPO"
 helm2 repo update
 
 export XDG_DATA_HOME=/helm3home
-helm3 repo add gcs-repo $GCS_HELM_REPO
+helm3 repo add gcs-repo "$GCS_HELM_REPO"
 helm3 repo update
 
-gcloud container clusters get-credentials $CLUSTER --zone $ZONE --project $PROJECT
+gcloud container clusters get-credentials "$CLUSTER" --zone "$ZONE" --project "$PROJECT"
 
 export RELEASE_NAME=$(/applicationName.sh "$HELM_ARGS" )
 
