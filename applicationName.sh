22c3d6b225a8d9cc9dd96c8d6041d9bb7b6888a1
Arvid Widerberg <59437103+arvidviderberg@users.noreply.github.com>
2020-05-07 07:56:01 +0200

    Add repo label and refactor for new name label (#12)
    * Add repo label
    
    * Add tests to all branches
    
    * Update application name script
    
    * Add more tests

--- applicationName.sh
+++ applicationName.sh
@@ -15,27 +15,42 @@ for i in "${!array[@]}"; do
     export APPLICATION_NAME=$(echo $value | sed 's/applicationName=//g')
     echo $APPLICATION_NAME
     exit 0
+  elif [[ $value == application.name=* ]]
+  then
+    export APPLICATION_NAME=$(echo $value | sed 's/application.name=//g')
+    echo $APPLICATION_NAME
+    exit 0
   fi
 done
 
 eval "fileArray=($FILES)";
 
-if [ ${#fileArray[@]} == 0 ]
+if [ ${#fileArray[@]} = 0 ]
 then
   echo "NO APPLICATION_NAME :("
   exit 1
 fi
 
-if [ ${#fileArray[@]} == 1 ]
+# If more than one values file, reverse the order and merge them in a temporary file.
+if [ ${#fileArray[@]} -gt 1 ]
 then
-  APPLICATION_NAME=$(cat $FILES | yq r - applicationName)
-else
   REVERSED_FILES=$(echo $FILES | awk '{ for (i=NF; i>1; i--) printf("%s ",$i); print $1; }')
-  APPLICATION_NAME=$(yq m $REVERSED_FILES | yq r - applicationName)
+  yq m $REVERSED_FILES > "tmp.yaml"
+  VALUES="tmp.yaml"
+else
+  VALUES=$FILES
 fi
 
-if [ $APPLICATION_NAME == "" ]
+# Check if new or old naming convention exists.
+OLD_NAME=$(cat $VALUES | yq r - applicationName)
+NEW_NAME=$(cat $VALUES | yq r - application.name)
+if [ ! $OLD_NAME = "null" ]
 then
+  export APPLICATION_NAME=$OLD_NAME
+elif [ ! $NEW_NAME = "null" ]
+then
+  export APPLICATION_NAME=$NEW_NAME
+else
   echo "NO APPLICATION_NAME :("
   exit 1
 fi
--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,13 @@ export GCS_HELM_REPO="$INPUT_GCS_HELM_REPO"
 export HELM_VERSION="$INPUT_HELM_VERSION"
 export HELM_ARGS="$INPUT_HELM_ARGS"
 export HELM_COMMAND="$INPUT_HELM_COMMAND"
+export REPOSITORY=$(eval "REPO=$INPUT_REPOSITORY" && echo $REPO | sed 's/\//__/') # Replace '/' with '__', since labels don't support '/'
 
 echo -----
 echo "$ZONE"
 echo "$PROJECT"
 echo "$CLUSTER"
+echo "$REPOSITORY"
 echo "$GCS_HELM_REPO"
 echo "$HELM_VERSION"
 echo "$HELM_ARGS"
@@ -38,10 +40,10 @@ echo "installing $RELEASE_NAME"
 
 if [ $HELM_VERSION == '2' ]
 then
-  helm2 $(eval "array=($HELM_COMMAND)"; for arg in "${array[@]}"; do echo "$arg"; done)
+  helm2 $(eval "array=($HELM_COMMAND)"; for arg in "${array[@]}"; do echo "$arg"; done )
 elif [ $HELM_VERSION == '3' ]
 then
-  helm3 $(eval "array=($HELM_COMMAND)"; for arg in "${array[@]}"; do echo "$arg"; done)
+  helm3 $(eval "array=($HELM_COMMAND)"; for arg in "${array[@]}"; do echo "$arg"; done )
 else
   echo "helm version $VERSION unsupported"
   exit 1
