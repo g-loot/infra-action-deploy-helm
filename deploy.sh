d90bf6c0dd3f130c4b5fc68bbdba08aa041d0104
Arvid Widerberg <arvid.viderberg@gloot.com>
2020-05-18 10:17:05 +0200

    Add support for custom charts

--- deploy.sh
+++ deploy.sh
@@ -4,32 +4,34 @@ export GCP_KEY="$INPUT_GCP_KEY"
 export ZONE="$INPUT_ZONE"
 export PROJECT="$INPUT_PROJECT"
 export CLUSTER="$INPUT_CLUSTER"
-export GCS_HELM_REPO="$INPUT_GCS_HELM_REPO"
+export HELM_REPO="$INPUT_HELM_REPO"
 export HELM_VERSION="$INPUT_HELM_VERSION"
 export HELM_ARGS="$INPUT_HELM_ARGS"
+export HELM_CHART="$INPUT_HELM_CHART"
 export HELM_COMMAND="$INPUT_HELM_COMMAND"
-export REPOSITORY=$(eval "REPO=$INPUT_REPOSITORY" && echo $REPO | sed 's/\//__/') # Replace '/' with '__', since labels don't support '/'
+export SERVICE_REPO=$(eval "REPO=$INPUT_SERVICE_REPO" && echo $REPO | sed 's/\//__/') # Replace '/' with '__', since labels don't support '/'
 
 echo -----
-echo "$ZONE"
-echo "$PROJECT"
-echo "$CLUSTER"
-echo "$REPOSITORY"
-echo "$GCS_HELM_REPO"
-echo "$HELM_VERSION"
-echo "$HELM_ARGS"
-echo "$HELM_COMMAND"
+echo "Zone: $ZONE"
+echo "Project: $PROJECT"
+echo "Cluster: $CLUSTER"
+echo "Service repo: $SERVICE_REPO"
+echo "Helm repo: $HELM_REPO"
+echo "Helm version: $HELM_VERSION"
+echo "Helm args: $HELM_ARGS"
+echo "Helm chart: $HELM_CHART"
+echo "Helm command: $HELM_COMMAND"
 echo -----
 
 echo "$GCP_KEY" | base64 -d > /tmp/google_credentials.json
 gcloud auth activate-service-account --key-file /tmp/google_credentials.json
 
 export HELM_HOME=/helm2home
-helm2 repo add gcs-repo "$GCS_HELM_REPO"
+helm2 repo add repo "$HELM_REPO"
 helm2 repo update
 
 export XDG_DATA_HOME=/helm3home
-helm3 repo add gcs-repo "$GCS_HELM_REPO"
+helm3 repo add repo "$HELM_REPO"
 helm3 repo update
 
 gcloud container clusters get-credentials "$CLUSTER" --zone "$ZONE" --project "$PROJECT"
