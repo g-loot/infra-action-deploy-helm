dad715ca9a06ecef911def74d624028683da4994
Arvid Widerberg <arvid.viderberg@gloot.com>
2021-04-20 15:35:48 +0200

    More debugging

--- deploy.sh
+++ deploy.sh
@@ -30,7 +30,7 @@ if [ $INPUT_HELM_VERSION == '3' ]
 then
   helm_command=$(eval "echo $INPUT_HELM_COMMAND")
   helm_args=$(eval "echo $INPUT_HELM_ARGS")
-  echo "helm $INPUT_HELM_COMMAND $INPUT_HELM_ARGS"
+  echo "Running: helm $helm_command $helm_args"
   helm $helm_command $helm_args
 else
   echo "helm version $INPUT_HELM_VERSION unsupported"
