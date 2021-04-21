fcdbd18a78848a3a75d769fe30aff0ddbf83b142
Andrej Pregl <andrej@gloot.com>
2021-04-21 09:56:40 +0200

    Avoid helm args eval to keep single quotes

--- deploy.sh
+++ deploy.sh
@@ -29,9 +29,8 @@ echo "installing $RELEASE_NAME"
 if [ $INPUT_HELM_VERSION == '3' ]
 then
   helm_command=$(eval "echo $INPUT_HELM_COMMAND")
-  helm_args=$(eval "echo $INPUT_HELM_ARGS")
-  echo "Running: helm $helm_command $helm_args"
-  helm $helm_command $helm_args
+  echo "Running: helm $helm_command $INPUT_HELM_ARGS"
+  helm $helm_command $INPUT_HELM_ARGS
 else
   echo "helm version $INPUT_HELM_VERSION unsupported"
   exit 1
