58767d9e65a6757951e705ee38da850084d7bb9a
esmaeilram <esmaeil.ramnejadian@gloot.com>
2022-06-21 17:25:50 +0200

    helm version 3.9.0 oci docker image

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ RUN apk add --no-cache openssl
 # Install Helm
 RUN curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
 RUN chmod +x get_helm.sh
-RUN ./get_helm.sh -v v3.4.1
+RUN ./get_helm.sh -v v3.9.0
 # Create Helm folder
 RUN mkdir /helm3home
 RUN chmod 775 -R /helm3home
@@ -20,9 +20,6 @@ RUN chmod +x /usr/local/bin/yq
 RUN gcloud components install kubectl
 # Copy scripts and make executable
 COPY deploy.sh /deploy.sh
-COPY applicationName.sh /applicationName.sh
-RUN chmod +x /deploy.sh && chmod +x /applicationName.sh
-
 WORKDIR /github/workspace/
 
 ENTRYPOINT [ "/deploy.sh" ]
--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,9 @@ export REPOSITORY=$(eval "REPO=$INPUT_REPOSITORY" && echo $REPO | sed 's/\//__/'
 
 echo "----- Helm Info -----"
 echo "Project: $INPUT_PROJECT"
-echo "Zone: $INPUT_ZONE"
-echo "Cluster: $INPUT_CLUSTER"
-echo "Git Repo: $REPOSITORY"
 echo "Helm Repo: $INPUT_GCS_HELM_REPO"
 echo "Helm Version: $INPUT_HELM_VERSION"
 echo "Helm Command: $INPUT_HELM_COMMAND"
-echo "Helm Args: $INPUT_HELM_ARGS"
 echo "-----"
 
 echo "$INPUT_GCP_KEY" | base64 -d > /tmp/google_credentials.json
@@ -19,23 +15,3 @@ gcloud auth activate-service-account --key-file /tmp/google_credentials.json
 export XDG_DATA_HOME=/helm3home
 helm repo add gcs-repo "$INPUT_GCS_HELM_REPO"
 helm repo update
-
-gcloud container clusters get-credentials "$INPUT_CLUSTER" --zone "$INPUT_ZONE" --project "$INPUT_PROJECT"
-
-export RELEASE_NAME=$(/applicationName.sh "$INPUT_HELM_ARGS")
-echo "Release name: $RELEASE_NAME"
-
-timestamp=$(date -d@"$(( `date +%s`+300))" '+%FT%T.3Z')
-log_url="https://console.cloud.google.com/logs/query;query=resource.labels.project_id%3D%22$INPUT_PROJECT%22%0Aresource.labels.location%3D%22$INPUT_ZONE%22%0Aresource.labels.cluster_name%3D%22$INPUT_CLUSTER%22%0A%22$RELEASE_NAME%22;cursorTimestamp=$timestamp?project=$INPUT_PROJECT"
-echo "GCP logs:  $log_url"
-echo "::set-output name=log_url::$log_url"
-
-if [ $INPUT_HELM_VERSION == '3' ]
-then
-  helm_command_array=($(eval "echo $INPUT_HELM_COMMAND"))
-  declare -a 'helm_args_array=('"$INPUT_HELM_ARGS"')'
-  helm "${helm_command_array[@]}" "${helm_args_array[@]}"
-else
-  echo "helm version $INPUT_HELM_VERSION unsupported"
-  exit 1
-fi
