37c6cf4f1a5aa7ac546443e098619a1006c4f45c
esmaeilram <esmaeil.ramnejadian@gloot.com>
2022-06-22 17:35:58 +0200

    add helm chart

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,8 @@ echo "$INPUT_GCP_KEY" | base64 -d > /tmp/google_credentials.json
 gcloud auth activate-service-account --key-file /tmp/google_credentials.json
 
 export XDG_DATA_HOME=/helm3home
-helm repo add gcs-repo "$INPUT_GCS_HELM_REPO"
-helm repo update
+#helm repo add gcs-repo "$INPUT_GCS_HELM_REPO"
+#helm repo update
 if [ $INPUT_HELM_VERSION == '3.9.0' ]
 then
   helm_command_array=($(eval "echo $INPUT_HELM_COMMAND"))
