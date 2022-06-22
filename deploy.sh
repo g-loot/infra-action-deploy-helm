3bd59dfdcc542f7a18f1d1bfb861edbfccbb5e95
esmaeilram <esmaeil.ramnejadian@gloot.com>
2022-06-22 16:37:06 +0200

    add helm command to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,3 +15,12 @@ gcloud auth activate-service-account --key-file /tmp/google_credentials.json
 export XDG_DATA_HOME=/helm3home
 helm repo add gcs-repo "$INPUT_GCS_HELM_REPO"
 helm repo update
+if [ $INPUT_HELM_VERSION == '3.9.0' ]
+then
+  helm_command_array=($(eval "echo $INPUT_HELM_COMMAND"))
+  declare -a 'helm_args_array=('"$INPUT_HELM_ARGS"')'
+  helm "${helm_command_array[@]}" "${helm_args_array[@]}"
+else
+  echo "helm version $INPUT_HELM_VERSION unsupported"
+  exit 1
+fi
