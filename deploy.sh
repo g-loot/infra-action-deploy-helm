a6ecf75ad51c908b9b32917b6905c5e99aa835f4
Warrior0078 <lokesh.bejjagam@gloot.com>
2022-08-12 13:57:57 +0200

    solving all lint errors

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-export REPOSITORY=$(eval "REPO=$INPUT_REPOSITORY" && echo $REPO | sed 's/\//__/') # Replace '/' with '__', since labels don't support '/'
+local -r export REPOSITORY=$(eval "REPO=$INPUT_REPOSITORY" && echo $REPO | sed 's/\//__/') # Replace '/' with '__', since labels don't support '/'
 
 echo "----- Helm Info -----"
 echo "Project: $INPUT_PROJECT"
@@ -20,18 +20,18 @@ gcloud auth activate-service-account --key-file /tmp/google_credentials.json
 export XDG_DATA_HOME=/helm3home
 helm pull "$INPUT_OCI_HELM_REPO" --version "$INPUT_OCI_CHART_VERSION"
 #helm repo update
-gcloud auth configure-docker 
+gcloud auth configure-docker
 gcloud container clusters get-credentials "$INPUT_CLUSTER" --zone "$INPUT_ZONE" --project "$INPUT_PROJECT"
 
-export RELEASE_NAME=$(/applicationName.sh "$INPUT_HELM_ARGS")
+local -r export RELEASE_NAME=$(/applicationName.sh "$INPUT_HELM_ARGS")
 echo "Release name: $RELEASE_NAME"
 
-timestamp=$(date -d@"$(( `date +%s`+300))" '+%FT%T.3Z')
+timestamp="$(date -d@"$(( $(date+%s)+300))" '+%FT%T.3Z')"
 log_url="https://console.cloud.google.com/logs/query;query=resource.labels.project_id%3D%22$INPUT_PROJECT%22%0Aresource.labels.location%3D%22$INPUT_ZONE%22%0Aresource.labels.cluster_name%3D%22$INPUT_CLUSTER%22%0A%22$RELEASE_NAME%22;cursorTimestamp=$timestamp?project=$INPUT_PROJECT"
 echo "GCP logs:  $log_url"
 echo "::set-output name=log_url::$log_url"
 
 
-helm_command_array=($(eval "echo $INPUT_HELM_COMMAND"))
+HELM_COMMAND_ARRAY=($(eval "echo $INPUT_HELM_COMMAND"))
 declare -a 'helm_args_array=('"$INPUT_HELM_ARGS"')'
-helm "${helm_command_array[@]}" "${helm_args_array[@]}"
+helm "${HELM_COMMAND_ARRAY[@]}" "${helm_args_array[@]}"
